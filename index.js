3bac0bb02b85f6afce2cade29bfceee95c506a85
Stephen <stephen@whitebolt.net>
2018-06-18 21:01:18 +0100

    Default export of gulp loader
    Add taskTree to default module export.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 'use strict';
 
-module.exports = {};
+const {getGulp} = require('./lib/util');
+const {taskTree} = require('./lib/taskTree');
+
+module.exports = (roots=[], options={})=>{
+	const gulp = options.gulp || getGulp(4);
+	return taskTree(roots, {...options, gulp})
+};
